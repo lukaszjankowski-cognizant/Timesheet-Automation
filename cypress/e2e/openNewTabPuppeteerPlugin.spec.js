56adb8bc319d5d5d3ab755c9275549b27cdfbc13
Topczewska <2373060@cognizant.com>
2024-12-20 13:30:50 +0100

    Added correct selector to use puppeter plugin in new tab

--- cypress/e2e/openNewTabPuppeteerPlugin.spec.js
+++ cypress/e2e/openNewTabPuppeteerPlugin.spec.js
@@ -3,38 +3,25 @@
 Cypress.on('uncaught:exception', (err, runnable) => {
   return false;
 });
- 
-
 
 describe('Azure Active Directory Authentication', () => {
-    /*beforeEach(() => {
-      
-      cy.loginToAAD(Cypress.env('aad_username'), Cypress.env('aad_password'));
-      //cy.visit('https://onecognizant.cognizant.com')
-      
-    });*/
 
-    it('enters page and logs with MS account', () => {
+    it('enter page and logs with MS account', () => {
      
       cy.loginToAAD(Cypress.env('aad_username'), Cypress.env('aad_password'));
-      cy.visit('https://onecognizant.cognizant.com/Home')
+
+      cy.visit('https://onecognizant.cognizant.com/Home');
       
       cy
       .get('#div_qkActioncontainer > div > div:nth-child(2) > div > div.row.row1 > div:nth-child(1)')
       .should('exist')
-      .click({force: true})
+      .click({force: true});
+
+      cy.wait(40000);//ms
 
       cy
       .puppeteer('switchToTabAndGetContent')
-      .should('exist')
-
-      cy.title().should('include', 'OneCognizant')
-    
-
-      
-      //cy.visit('https://compass.esa.cognizant.com')
-
-      //cy.contains('My Homepage').should('exist')
+      .should('exist');
       
     });
 });
